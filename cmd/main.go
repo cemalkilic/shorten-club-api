a8dda4ab03a40cda026233be475d0992282a65b8
Cemal Kılıç <cemalkilic96@gmail.com>
2021-02-06 01:38:54 +0300

    Minimal CORS for now

--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,28 @@ import (
     "github.com/cemalkilic/shorten-backend/middlewares"
     "github.com/cemalkilic/shorten-backend/service"
     "github.com/cemalkilic/shorten-backend/utils/validator"
-    "github.com/gin-contrib/cors"
     "github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
+}
+
 func main() {
     router := gin.Default()
-    router.Use(cors.Default())
+    router.Use(CORSMiddleware())
 
 
     cfg, _ := config.LoadConfig(".")
